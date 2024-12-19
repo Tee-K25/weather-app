0621bc066f8ca006872db6415538e1b4f8b037ec
Tee-K25 <zanytony111@gmail.com>
2024-12-19 16:43:22 +0300

    Made icon dynamic

--- src/home/index.jsx
+++ src/home/index.jsx
@@ -1,18 +1,62 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import logo from "../assets/weather-svgrepo-com.svg";
 import useFetchHook from "../components/use-fetch-hook";
 import Loading from "../components/loading";
+import Clear from "../assets/dynamicW/sun.png";
+import Clouds from "../assets/dynamicW/cloudy.png";
+import Rain from "../assets/dynamicW/rainy-day.png";
+import Snow from "../assets/dynamicW/snow.png";
+import Mist from "../assets/dynamicW/mist.png";
+import Thunderstorm from "../assets/dynamicW/thunderstrom.png";
 
 export default function Weather() {
   const [input, setInput] = useState("");
   const [search, setSearch] = useState("Nairobi");
-
+  const [keyWord, setKeyWord] = useState(null);
+  const words = ["Clear", "Clouds", "Thunderstorm", "Rain", "Snow", "Mist"];
   // utility hook
   const { data, loading, errorMsg } = useFetchHook(
     `https://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=fc4808a791e4074257e0377bdb26a2cc`
   );
 
-  console.log(data);
+  // utility function
+  function checkWeather() {
+    if (data) {
+      const apiWord = data.weather[0].main;
+      const weatherCondition = words.find((item) => {
+        return item === apiWord;
+      });
+      switch (weatherCondition) {
+        case "Clear":
+          setKeyWord(Clear);
+          break;
+        case "Clouds":
+          setKeyWord(Clouds);
+          break;
+        case "Thunderstorm":
+          setKeyWord(Thunderstorm);
+          break;
+        case "Rain":
+          setKeyWord(Rain);
+          break;
+        case "Snow":
+          setKeyWord(Snow);
+          break;
+        case "Mist":
+          setKeyWord(Mist);
+          break;
+        default:
+          setKeyWord(Clear);
+          break;
+      }
+    }
+  }
+
+  useEffect(() => {
+    checkWeather();
+  }, [data]);
+
+  console.log(keyWord);
   // handler functions
   function handleSubmit(e) {
     e.preventDefault();
@@ -62,7 +106,7 @@ export default function Weather() {
       ) : (
         <main>
           <div className="weather-icon">
-            <img src="./assets/icons8-sun-240.png" alt="" />
+            <img src={keyWord} alt="" />
           </div>
           <div className="weather-details">
             <div className="weather-details2">
